4d1021bd064b8b8a9e6deab3fe77291bb51db3fe
Winston <winstonmendezft@gmail.com>
2021-06-22 17:35:33 -0400

    Primer commit

---
+++ src/classes/todo.class.js
@@ -0,0 +1,29 @@
+
+export class Todo {
+
+    static fromJson({ id, tarea, completado, creado }) {
+
+        const tempTodo = new Todo( tarea );
+
+        tempTodo.id = id;
+        tempTodo.completado = completado;
+        tempTodo.creado = creado;
+
+        return tempTodo;
+    }
+
+    constructor( tarea ) {
+
+        this.tarea      = tarea;
+
+        this.id         = new Date().getTime(); //1232141242353
+        this.completado = false;
+        this.creado     = new Date();
+
+    }
+
+    imprimirClase() {
+        console.log(`${this.tarea} - ${this.id}`);
+    }
+
+}
